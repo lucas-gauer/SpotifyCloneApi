d9ed7e8559f43fad6536abfe5cb7b266f31fa796
Lucas Gauer <lucasfgauer@gmail.com>
2021-05-19 13:15:00 -0300

    Add 'stopAtFirstError' and 'whitelist' options to validation pipes

--- src/validation/pipes.ts
+++ src/validation/pipes.ts
@@ -2,9 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 export const ValidateCreate = new ValidationPipe({
   transform: true,
+  stopAtFirstError: true,
+  whitelist: true,
 });
 
 export const ValidateUpdate = new ValidationPipe({
   transform: true,
   skipMissingProperties: true,
+  stopAtFirstError: true,
+  whitelist: true,
 });
